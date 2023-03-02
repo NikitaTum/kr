675489336165044e1a7cdcfc71395ec53b98ae61
Nikita Tum <nikita.petrov6663@mail.ru>
2023-03-02 19:42:02 +0300

    2

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,3 +1,11 @@
 ﻿string[] array1 = new string[5] {"Hi", "5", "tesson", "5543", "in",};
 string[] array2 = new string[array1.Length];
 void ArraySecond(string[] array1, string[] array2)
+{
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+if(array1[i].Length <= 3)
+{
+array2[count] = array1[i];
+count++;
